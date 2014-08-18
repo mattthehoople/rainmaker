f0abf17c047bf892a9859cafbdeb5b2d719401b5
Matt Tate <mjtate@gmail.com>
2014-08-18 15:25:03 +0100

    Clear central panel correctly. Minor template changes.

--- app/js/main.js
+++ app/js/main.js
@@ -14,7 +14,7 @@ var Router = Backbone.Router.extend({
     },
 
     home: function () {
-        $("#center").empty();
+        $(".panel").empty();
         var cards = new CardStateCollection();
         var cardsView = new CardStateListView({collection:cards});
         cards.fetch({
@@ -25,7 +25,7 @@ var Router = Backbone.Router.extend({
     },
 
     burndown: function () {
-        $("#center").empty();
+        $(".panel").empty();
         var actions = new ActionCollection();
 
         var completedTasks = new CompletedTasksView({collection:actions});
